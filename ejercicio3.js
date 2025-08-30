f25b464204fb78a0958751218f6a915849dccb3c
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-30 18:06:04 +0200

    Delete ejercicio3.js

--- ejercicio3.js
+++
@@ -1,7 +0,0 @@
-// Definición de variables
-var nombre = "Leia";
-var apellido = "Organa";
-var edad = 20;
-
-// Mensaje de concatenación
-console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan.");

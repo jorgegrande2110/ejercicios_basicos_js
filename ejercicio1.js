1f7dd352ba98109b038a08094e110cd0f0f0ac45
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-30 18:06:54 +0200

    Create ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,19 @@
+// 1. Superhéroe favorito
+let miSuperheroeFavorito = "Hulk";
+
+// 2. Número favorito
+let numeroFavorito = 50;
+
+// 3. Dimensiones
+let altura = 5;
+let longitud = 10;
+
+// 4. Suma de las dimensiones
+let suma = altura + longitud;
+
+// 5. Imprimir resultados
+console.log("miSuperheroeFavorito:", miSuperheroeFavorito);
+console.log("numeroFavorito:", numeroFavorito);
+console.log("altura:", altura);
+console.log("longitud:", longitud);
+console.log("suma:", suma);

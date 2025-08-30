3ad5d8925e3a55ff2c109b2623b6a74e807449be
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-30 18:24:51 +0200

    Create ejercicio4.js

---
+++ ejercicio4.js
@@ -0,0 +1,22 @@
+const aldeanos = ["Fibrilio", "Narciso", "Vacarena", "Tendo", "Nendo"];
+
+// 4.1 - Saca a "Tendo" por consola atacando su posición.
+console.log(aldeanos[3]); // Tendo
+
+// 4.2 - Coloca en el último lugar de este array a "Cervasio".
+aldeanos.push("Cervasio");
+
+// 4.3 - Cambia el primer elemento de este array por "Bambina".
+aldeanos[0] = "Bambina";
+
+// 4.4 - Dale la vuelta a este array.
+aldeanos.reverse();
+
+// 4.5 - Cambia a "Narciso" por "Canela" haciendo uso de un método de array.
+const indexNarciso = aldeanos.indexOf("Narciso");
+if (indexNarciso !== -1) {
+  aldeanos.splice(indexNarciso, 1, "Canela");
+}
+
+// 4.6 - Imprime por consola el último elemento sin usar índice explícito.
+console.log(aldeanos[aldeanos.length - 1]);

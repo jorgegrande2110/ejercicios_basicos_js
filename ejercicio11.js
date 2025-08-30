7cebc9e57218e804ac532bcb57c20f0e34af9f24
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-30 18:31:34 +0200

    Create ejercicio11.js

---
+++ ejercicio11.js
@@ -0,0 +1,15 @@
+const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+
+function averageWord(list) {
+  let sum = 0;
+  for (let item of list) {
+    if (typeof item === 'number') {
+      sum += item;
+    } else if (typeof item === 'string') {
+      sum += item.length;
+    }
+  }
+  return sum / list.length;
+}
+
+console.log(averageWord(mixedElements)); 

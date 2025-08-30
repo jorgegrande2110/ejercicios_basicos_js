3e2f8e466bdba3fea35e31e8a3f0cb57a8641308
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-30 18:11:44 +0200

    Create ejercicio3.js

---
+++ ejercicio3.js
@@ -0,0 +1,22 @@
+// 3.1 Multiplicar 10 por 5 y mostrar resultado
+console.log(10 * 5); // 50
+
+// 3.2 Dividir 10 por 2 y mostrar resultado
+console.log(10 / 2); // 5
+
+// 3.3 Mostrar el resto de dividir 15 por 9
+console.log(15 % 9); // 6
+
+// 3.4 Operador de asignación para o = 15, con p=10 y j=5
+let p = 10;
+let j = 5;
+let o = p + j;  // o = 15
+
+console.log(o);
+
+// 3.5 Operador de asignación para i = 50, con c=10 y m=5
+let c = 10;
+let m = 5;
+let i = c * m;  // i = 50
+
+console.log(i);
